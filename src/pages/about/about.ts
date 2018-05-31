6ae047001b4bc2c0c088eb1977c4af8f0aebc12c
cskrada <frada@overcom.net>
2018-05-31 11:49:45 -0400

    la pagina de about se aplico una lista DINAMICA sencilla utilizando las directivas *ngFor y la propiedad de las {{}} desde HTML y se creo un arreglo desde typesript

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -6,7 +6,22 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'about.html'
 })
 export class AboutPage {
-
+  
+  list: Array<any> = [
+  	{
+	  titulo: "Perro",
+	  color: "#444"
+  	},
+  	{
+	  titulo: "Gato",
+	  color: "#666"
+  	},
+  	{
+	  titulo: "Raton",
+	  color: "#888"
+  	}
+  ];
+  
   constructor(public navCtrl: NavController) {
 
   }
