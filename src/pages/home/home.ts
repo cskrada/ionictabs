dacc44a9984bdaaf732aed59a44340470a2c8486
cskrada <frada@overcom.net>
2018-05-31 12:10:19 -0400

    en Home.ts se debe importar la clase masinfo para hacer el enlace

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { MasinfoPage } from '../masinfo/masinfo';
 
 @Component({
   selector: 'page-home',
